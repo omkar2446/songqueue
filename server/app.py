47a051af242378d2573443fface13fccffe9442f
omkar_2446 <otambe655@gmail.com>
2026-04-15 20:54:42 +0530

    SOLVE RELODE PROBLEM

--- server/app.py
+++ server/app.py
@@ -775,8 +775,8 @@ def get_youtube_url(video_id):
             'socket_timeout': 10,
             'source_address': '0.0.0.0', 
             'geo_bypass': True,
-            # Use embedded player client which is often less restricted
-            'extractor_args': {'youtube': {'player_client': ['web_embedded']}},
+            # Use TV client which has minimal bot detection
+            'extractor_args': {'youtube': {'player_client': ['tv']}},
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -866,7 +866,7 @@ def resolve_spotify():
             'noplaylist': True,
             'skip_download': True,
             'source_address': '0.0.0.0', 
-            'extractor_args': {'youtube': {'player_client': ['web_embedded']}},
+            'extractor_args': {'youtube': {'player_client': ['tv']}},
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
